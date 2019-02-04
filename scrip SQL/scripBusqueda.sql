e1bb50287822f5137de317e62fd5ebd660845bf9
Erick Humberto Cordova Gavilanes <erihucor@espol.edu.ec>
2019-02-04 10:23:21 -0500

    reparando diagrama de clases de uso

--- scrip SQL/scripBusqueda.sql
+++ scrip SQL/scripBusqueda.sql
@@ -1,2 +1,15 @@
 use proyectods;
+
 select * from Producto order by porcentajeBusqueda desc limit 5;
+
+create view todasLasVentas as
+select count(idVenta) from venta; 
+
+
+DELIMITER |
+CREATE TRIGGER actualizaPorcentaje AFTER INSERT ON venta
+FOR EACH ROW BEGIN
+UPDATE venta set porcentajeVenta =  (select count(idVenta) from Venta inner join pedido on idPedido=idPedido inner join  productoxPedido on idPedido =idPedido inner join Producto on idProducto = idProducto where idProducto = idProducto.new ) /todasLasVentas ;
+END;
+|
+DELIMITER ;
